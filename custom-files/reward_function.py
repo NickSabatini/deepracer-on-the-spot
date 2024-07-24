344f3e5554f5a6d072b4a2f9153166e886d55a63
Nick Sabatini <nicksabatini@Nicks-MacBook-Pro.local>
2024-07-24 17:32:14 -0400

    revamped reward

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -11,35 +11,26 @@ def reward_function(params):
     steps = params['steps']
     progress = params['progress']
     all_wheels_on_track = params['all_wheels_on_track']
-    waypoints = params['waypoints']
-    closest_waypoints = params['closest_waypoints']
-    heading = params['heading']
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+
     reward = 1e-3
 
-    # Progress per step reward is large, this is a proxy for quick completion of the track
-    reward += progress * 4
+    # Progress
+    reward += (progress / 100) * 4
 
-    # reward for speed
+    # reward for speed (0-4)
     reward += speed
 
-    # Reward for velocity vector alignment with direction of track
-    # Calculate the direction of the center line based on two closest waypoints
-    waypoint_1 = waypoints[closest_waypoints[0]]
-    waypoint_2 = waypoints[closest_waypoints[1]]
-    
-    track_direction = math.atan2(waypoint_2[1] - waypoint_1[1], waypoint_2[0] - waypoint_1[0])
-    track_direction = math.degrees(track_direction)
-    
-    # Calculate the difference between the track direction and the heading direction of the car
-    direction_diff = abs(track_direction - heading)
-    if direction_diff > 180:
-        direction_diff = 360 - direction_diff
-    
-    # Reward function based on the direction difference
-    if direction_diff < 10:
-        reward += 4
+    # distance from center line reward
+    center_reward = (1 - (distance_from_center/(track_width*0.5))**2) * 4
+    reward += center_reward
+
+    # reward for finishing the track
+    if progress >= 100:
+        reward += 50
 
     if not all_wheels_on_track:
-        reward *= 0.5
+        reward = 1e-3
 
     return float(reward)
