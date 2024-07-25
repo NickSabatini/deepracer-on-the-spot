16bb6aeb8fc61f775b94ba769be5d9a432ed6564
Nick Sabatini <nicksabatini@Nicks-MacBook-Pro.local>
2024-07-25 10:05:41 -0400

    changed action space and tweaked reward function

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -8,26 +8,23 @@ def reward_function(params):
     track_width = params['track_width']
     steering = abs(params['steering_angle']) # Only need the absolute steering angle
 
-    # Calculate 3 marks that are farther and father away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    reward = 1
 
-    # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
-    else:
-        reward = 1e-3  # likely crashed/ close to off track
+    # Progress
+    reward += (progress / 100) * 2
 
-    # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 15
+    # reward for speed (0-2)
+    reward += speed * 0.5
 
-    # Penalize reward if the car is steering too much
-    if steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
+    # distance from center line reward
+    center_reward = (1 - (distance_from_center/(track_width*0.5))**2) * 5
+    reward += center_reward
+
+    # reward for finishing the track
+    if progress >= 100:
+        reward += 100
+
+    if not all_wheels_on_track:
+        reward = 1e-3
 
     return float(reward)
