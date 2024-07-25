693a99ee4d9a4662ccbf08888107f2cede3ae74c
Nick Sabatini <nicksabatini@Nicks-MacBook-Pro.local>
2024-07-25 10:08:03 -0400

    more reward tweaks

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -15,13 +15,13 @@ def reward_function(params):
     reward = 1
 
     # Progress
-    reward += (progress / 100) * 2
+    reward += (progress / 100) * 4
 
     # reward for speed (0-2)
     reward += speed * 0.5
 
     # distance from center line reward
-    center_reward = (1 - (distance_from_center/(track_width*0.5))**2) * 5
+    center_reward = (1 - (distance_from_center/(track_width*0.5))**2) * 10
     reward += center_reward
 
     # reward for finishing the track
